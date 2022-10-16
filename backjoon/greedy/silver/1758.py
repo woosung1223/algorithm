359cb27c99d91482f589e2437b5393333bb8d236
woosung1223 <dntjd991223@naver.com>
2022-10-16 14:03:18 +0900

    greedy: 1758

--- backjoon/greedy/silver/1758.py
+++ backjoon/greedy/silver/1758.py
@@ -4,8 +4,9 @@ user_input = []
 for i in range(N):
     user_input.append(int(input()))
 
-user_input.sort()
-
+user_input.sort(reverse=True)
+# 받은 등수는 점점 높아지고, 빼는 값도 점점 높아지는 구조이기 때문에
+# 역으로 정렬하면 됨! 어차피 음수가 되면 0이 되기 때문에
 result = 0
 for i in range(N):
     tip = user_input[i] - i
